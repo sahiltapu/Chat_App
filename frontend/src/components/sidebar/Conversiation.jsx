464e0b56e6351d5987b5d755e0b6a4d7e68639dc
sahiltapu <sahilmishranayagarh@gmail.com>
2024-02-12 23:22:05 +0530

    Some new features are added to handle the login and conversations

--- frontend/src/components/sidebar/Conversiation.jsx
+++ frontend/src/components/sidebar/Conversiation.jsx
@@ -1,28 +1,37 @@
-import React from "react";
+import { useSocketContext } from "../../context/SocketContext";
+import useConversation from "../../zustand/useConversation";
 
-const Conversiation = () => {
-  return (
-    <>
-      <div className="flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer">
-        <div className="avatar online">
-          <div className="w-12 rounded-full">
-            <img
-              src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
-              alt="user-avatar"
-            />
-          </div>
-        </div>
-        <div className="flex flex-col flex-1">
-            <div className="flex gap-3 justify-between">
-                <p className="font-bold text-gray-200">sahilmishra</p>
-                <span className="text-xl">@@</span>
-            </div>
+const Conversation = ({ conversation, lastIdx, emoji }) => {
+	const { selectedConversation, setSelectedConversation } = useConversation();
 
-        </div>
-      </div>
-      <div className="divider my-0 py-0 h-1" />
-    </>
-  );
+	const isSelected = selectedConversation?._id === conversation._id;
+	const { onlineUsers } = useSocketContext();
+	const isOnline = onlineUsers.includes(conversation._id);
+
+	return (
+		<>
+			<div
+				className={`flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer
+				${isSelected ? "bg-sky-500" : ""}
+			`}
+				onClick={() => setSelectedConversation(conversation)}
+			>
+				<div className={`avatar ${isOnline ? "online" : ""}`}>
+					<div className='w-12 rounded-full'>
+						<img src={conversation.profilePic} alt='user avatar' />
+					</div>
+				</div>
+
+				<div className='flex flex-col flex-1'>
+					<div className='flex gap-3 justify-between'>
+						<p className='font-bold text-gray-200'>{conversation.fullName}</p>
+						<span className='text-xl'>{emoji}</span>
+					</div>
+				</div>
+			</div>
+
+			{!lastIdx && <div className='divider my-0 py-0 h-1' />}
+		</>
+	);
 };
-
-export default Conversiation;
+export default Conversation;
--- frontend/src/components/sidebar/Conversiations.jsx
+++ frontend/src/components/sidebar/Conversiations.jsx
@@ -1,15 +1,22 @@
-import React from 'react'
-import Conversiation from './Conversiation'
+import useGetConversations from "../../hooks/useGetConversations";
+import { getRandomEmoji } from "../../utils/emojis";
+import Conversation from "./Conversiation"
 
-const Conversiations = () => {
-  return (
-    <div>
-      <div className='py-2 flex flex-col overflow-auto'>
-        <Conversiation />
+const Conversations = () => {
+	const { loading, conversations } = useGetConversations();
+	return (
+		<div className='py-2 flex flex-col overflow-auto'>
+			{conversations.map((conversation, idx) => (
+				<Conversation
+					key={conversation._id}
+					conversation={conversation}
+					emoji={getRandomEmoji()}
+					lastIdx={idx === conversations.length - 1}
+				/>
+			))}
 
-      </div>
-    </div>
-  )
-}
-
-export default Conversiations
+			{loading ? <span className='loading loading-spinner mx-auto'></span> : null}
+		</div>
+	);
+};
+export default Conversations;
