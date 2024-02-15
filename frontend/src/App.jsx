69d4aa70e5c799e8437985852f8c2c915b2561bc
sahiltapu <sahilmishranayagarh@gmail.com>
2024-02-15 23:35:47 +0530

    used Aceternity UI to design the new background

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,29 +6,32 @@ import Home from "./pages/home/Home";
 import { Navigate, Route, Routes } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import { useAuthContext } from "./context/AuthContext";
+import { WavyBackground } from "./components/ui/WaveBackground";
 
 function App() {
   const { authUser } = useAuthContext();
 
   return (
     <>
-      <div className="p-4 h-screen flex items-center justify-center">
-        <Routes>
-          <Route
-            path="/"
-            element={authUser ? <Home /> : <Navigate to={"/login"} />}
-          />
-          <Route
-            path="/login"
-            element={authUser ? <Navigate to="/" /> : <Login />}
-          />
-          <Route
-            path="/signup"
-            element={authUser ? <Navigate to="/" /> : <SignUp />}
-          />
-        </Routes>
-        <Toaster />
-      </div>
+      <WavyBackground>
+        <div className="p-4 h-screen flex items-center justify-center">
+          <Routes>
+            <Route
+              path="/"
+              element={authUser ? <Home /> : <Navigate to={"/login"} />}
+            />
+            <Route
+              path="/login"
+              element={authUser ? <Navigate to="/" /> : <Login />}
+            />
+            <Route
+              path="/signup"
+              element={authUser ? <Navigate to="/" /> : <SignUp />}
+            />
+          </Routes>
+          <Toaster />
+        </div>
+      </WavyBackground>
     </>
   );
 }
