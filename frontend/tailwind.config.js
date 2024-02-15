bb7862b21e4da299b7ceb6c217160147802c7d1d
sahiltapu <sahilmishranayagarh@gmail.com>
2024-02-15 22:32:50 +0530

    installed new packages for Aceternity UI

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -9,3 +9,30 @@ export default {
   },
   plugins: [require("daisyui")],
 }
+
+
+import defaultTheme from "tailwindcss/defaultTheme";
+import colors from "tailwindcss/colors";
+import { default as flattenColorPalette } from "tailwindcss/lib/util/flattenColorPalette";
+
+/** @type {import('tailwindcss').Config} */
+export const content = ["./src/**/*.{ts,tsx}"];
+export const darkMode = "class";
+export const theme = {
+  // rest of the code
+};
+export const plugins = [
+  // rest of the code
+  addVariablesForColors,
+];
+
+// This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
+function addVariablesForColors({ addBase, theme }) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+  addBase({
+    ":root": newVars,
+  });
+}
