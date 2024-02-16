6c4a67ed9bdeab254902f9f734a00d05a3593f50
sahiltapu <sahilmishranayagarh@gmail.com>
2024-02-16 13:20:37 +0530

    completed the app using socketio

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,5 +1,6 @@
 import Conversation from "../models/conversation.model.js";
 import Message from "../models/message.model.js";
+import { getReceiverSocketId, io } from "../socket/socket.js";
 
 export const sendMessage = async (req, res) => {
     try {
@@ -23,13 +24,19 @@ export const sendMessage = async (req, res) => {
         if (newMessage) {
             conversation.message.push(newMessage._id);
         }
-        // SOCKET IO functionality
-
         // await conversation.save();
         // await newMessage.save();
 
         // This will run in parallel
         await Promise.all([conversation.save(), newMessage.save()]);
+
+        // SOCKET IO functionality
+        const receiverSocketId = getReceiverSocketId(receiverId);
+        if(receiverSocketId){
+            io.to(receiverSocketId).emit("newMessage" , newMessage);
+        }
+
+
 
         res.status(201).json(newMessage);
     } catch (error) {
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
+// import path from "path";
 import express from "express";
-import { config } from "dotenv";
+import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.routes.js";
@@ -7,27 +8,28 @@ import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
+import { app, server } from "./socket/socket.js";
 
-const app = express();
 const PORT = process.env.PORT || 5000;
 
-config();
+// const __dirname = path.resolve();
 
-app.use(express.json()); // to parse incoming requests with JSON payload (from req.body)
-app.use(cookieParser()); // to parse incoming cookie from req.cookies
+dotenv.config();
+
+app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
+// app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-// app.get("/", (req, res) => {
-//     //root route http://localhost:5000/
-//     res.send("hello sahil");
+// app.get("*", (req, res) => {
+// 	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 // });
 
-
-app.listen(PORT, () => {
-    connectToMongoDB();
-    console.log(`Server running on port http://localhost:${PORT}/`);
+server.listen(PORT, () => {
+	connectToMongoDB();
+	console.log(`Server Running on port  http://localhost:${PORT}`);
 });
